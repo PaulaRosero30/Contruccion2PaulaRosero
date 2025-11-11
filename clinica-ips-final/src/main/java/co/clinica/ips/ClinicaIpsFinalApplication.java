44affa009027de7d355a385ac5c4c6bec1229f25
PaulaRosero30 <paulaandrea1595.rosero@gmail.com>
2025-11-11 14:10:22 -0500

    inital commit

---
+++ clinica-ips-final/src/main/java/co/clinica/ips/ClinicaIpsFinalApplication.java
@@ -0,0 +1,13 @@
+package co.clinica.ips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaIpsFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicaIpsFinalApplication.class, args);
+	}
+
+}
---
+++ clinica-ips-final/src/test/java/co/clinica/ips/ClinicaIpsFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package co.clinica.ips;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaIpsFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
